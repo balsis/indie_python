233cc928f5b4592adca8c57276e7e7327f69ff1e
artur.balsis <artur.balsis@gmail.com>
2024-07-23 18:07:27 +0300

    set continuation

--- set/debug.py
+++ set/debug.py
@@ -1,7 +1,11 @@
-set_a = {31, 37, 39, 41, 47, 58, 60, 62, 70, 75,
-         76, 77, 78, 79, 80, 81, 85, 86, 88, 90, 93, 96, 98, 99}
+words = ['mention', 'soup', 'pneumonia', 'tradition', 'concert', 'tease', 'generation',
+         'winter', 'national', 'jacket', 'winter', 'wrestle', 'proposal', 'error', 
+         'pneumonia', 'concert', 'value', 'value', 'disclose', 'glasses', 'tank',
+         'national', 'soup', 'feel', 'few', 'concert', 'wrestle', 'proposal', 'soup',
+         'sail', 'brown', 'service', 'proposal', 'winter', 'jacket', 'mention', 'tradition',
+         'value', 'feel', 'bear', 'few', 'value', 'winter', 'proposal', 'government', 
+         'control', 'value', 'few', 'generation', 'service', 'national',
+         'tradition', 'government', 'mention', 'proposal']
 
-set_b = {0, 1, 8, 16, 17, 18, 22, 24, 29, 31,
-         33, 34, 36, 42, 46, 47, 51, 53, 62, 64, 65, 66, 67}
-set_inter = set_a & set_b
-print(len(set_inter))
+
+print(len({i for i in words if len(i) > 6}))
---
+++ set/operations with sets.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+for i in range(n):
+
+	a = input().split()
+	b = set(a)
+	print(len(set(a)))
+
