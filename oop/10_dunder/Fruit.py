458fd7fdb87e10b43bfdae5c4ce7087fe2357dd9
artur.balsis <artur.balsis@gmail.com>
2025-02-17 15:01:13 +0300

    add tasks

---
+++ oop/10_dunder/Fruit.py
@@ -0,0 +1,53 @@
+from functools import total_ordering
+
+
+@total_ordering
+class Fruit:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def __eq__(self, other):
+        if isinstance(other, Fruit):
+            return self.price == other.price
+        elif isinstance(other, (int, float)):
+            return self.price == other
+        return NotImplemented
+
+    def __lt__(self, other): # <
+        if isinstance(other, Fruit):
+            return self.price < other.price
+        elif isinstance(other, (int, float)):
+            return self.price < other
+        return NotImplemented
+
+
+
+
+# Проверка
+apple = Fruit("Apple", 0.5)
+orange = Fruit("Orange", 1)
+banana = Fruit("Banana", 1.6)
+lime = Fruit("Lime", 1.0)
+
+assert (banana > 1.2) is True
+assert (banana >= 1.2) is True
+assert (banana == 1.2) is False
+assert (banana != 1.2) is True
+assert (banana < 1.2) is False
+assert (banana <= 1.2) is False
+
+assert (apple > orange) is False
+assert (apple >= orange) is False
+assert (apple == orange) is False
+assert (apple != orange) is True
+assert (apple < orange) is True
+assert (apple <= orange) is True
+
+assert (orange == lime) is True
+assert (orange != lime) is False
+assert (orange > lime) is False
+assert (orange < lime) is False
+assert (orange <= lime) is True
+assert (orange >= lime) is True
+print('Good')
