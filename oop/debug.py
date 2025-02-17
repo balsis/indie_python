db4033b24696352b6584160320bbe1a0f4646152
artur.balsis <artur.balsis@gmail.com>
2025-02-17 16:19:41 +0300

    add tasks

--- oop/debug.py
+++ oop/debug.py
@@ -1,11 +1,19 @@
-class MyClass:
-    def __init__(self, x):
-        self.x = x
+def test_readable_function():
+    open_browser(browser_name="Chrome")
+    go_to_companyname_homepage(page_url="https://companyname.com")
+    find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
-    def __eq__(self, other):
-        return self.x is other.x
 
-a = MyClass([1, 2, 3])
-b = MyClass([1, 2, 3])
+def open_browser(browser_name):
+    actual_result = f"Open Browser [{browser_name}]"
+    assert actual_result == "Open Browser [Chrome]"
 
-print(a == b)
+
+def go_to_companyname_homepage(page_url):
+    actual_result = f"Go To Companyname Homepage [{page_url}]"
+    assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
+
+
+def find_registration_button_on_login_page(page_url, button_text):
+    actual_result = f"Find Registration Button On Login Page [{page_url}, {button_text}]"
+    assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
