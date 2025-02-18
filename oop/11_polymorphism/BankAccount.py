cf165c04186e48c6aa4ea3956f8b2f182fae622a
artur.balsis <artur.balsis@gmail.com>
2025-02-18 18:24:49 +0300

    add tasks

--- oop/11_polymorphism/BankAccount.py
+++ oop/11_polymorphism/BankAccount.py
@@ -1,7 +1,3 @@
-from functools import total_ordering
-
-
-@total_ordering
 class BankAccount:
     def __init__(self, name, balance):
         self.name = name
@@ -10,21 +6,36 @@ class BankAccount:
     def __str__(self):
         return self.name
 
-    def __eq__(self, other):
+    def __radd__(self, other):
         if isinstance(other, BankAccount):
-            return self.balance == other.balance
+            return self.balance + other.balance
+        elif isinstance(other, Numbers):
+            return sum(self.balance + value for value in other._values)
         elif isinstance(other, (int, float)):
-            return self.balance == other
-        return NotImplemented
-
-    def __lt__(self, other):  # <
-        if isinstance(other, BankAccount):
-            return self.balance < other.balance
-        elif isinstance(other, (int, float)):
-            return self.balance < other
+            return self.balance + other
         return NotImplemented
 
 
-values = [BankAccount('Petrovich', 400), 500, BankAccount('Andrey', 200), 100, BankAccount('Zina', 150)]
-values.sort()
-print(*values)
+class Numbers:
+    def __init__(self, values: list):
+        self._values = values
+
+    def __radd__(self, other):
+        if isinstance(other, BankAccount):
+            return sum(other.balance + value for value in self._values)
+        elif isinstance(other, Numbers):
+            return sum(self._values) + sum(other._values)
+        elif isinstance(other, (int, float)):
+            return sum(self._values) + other
+        return NotImplemented
+
+
+lst = [
+    Numbers([10, 20, 10]),
+    BankAccount('Ivan', 30),
+    Numbers([30, 40]),
+]
+
+# Для корректного сложения используем начальное значение 0
+result = sum(lst, 0)
+print(result)  # Ожидаем 140
