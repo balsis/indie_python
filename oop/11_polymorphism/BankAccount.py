4c84dc5862ca5b0c10c7c468ec11f9a951182d49
artur.balsis <artur.balsis@gmail.com>
2025-02-18 17:14:56 +0300

    add tasks

--- oop/11_polymorphism/BankAccount.py
+++ oop/11_polymorphism/BankAccount.py
@@ -1,3 +1,7 @@
+from functools import total_ordering
+
+
+@total_ordering
 class BankAccount:
     def __init__(self, name, balance):
         self.name = name
@@ -5,6 +9,20 @@ class BankAccount:
 
     def __str__(self):
         return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, BankAccount):
+            return self.balance == other.balance
+        elif isinstance(other, (int, float)):
+            return self.balance == other
+        return NotImplemented
+
+    def __lt__(self, other):  # <
+        if isinstance(other, BankAccount):
+            return self.balance < other.balance
+        elif isinstance(other, (int, float)):
+            return self.balance < other
+        return NotImplemented
 
 
 values = [BankAccount('Petrovich', 400), 500, BankAccount('Andrey', 200), 100, BankAccount('Zina', 150)]
