e77f178f3fca1ec4b4901170b21fa6bb9aadc675
artur.balsis <artur.balsis@gmail.com>
2026-08-06 16:47:23 +0300

    add tasks

--- basics/balakirev/decorator5.py
+++ basics/balakirev/decorator5.py
@@ -1,13 +1,27 @@
 t = {'ё': 'yo', 'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ж': 'zh',
      'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p',
-    'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh',
-    'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
+     'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh',
+     'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
 
-def transformation(n, sep = "-"):
+
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        res = func(*args, **kwargs)
+        while "--" in res:
+            res = res.replace("---", "-").replace("--", "-")
+        return res
+
+    return wrapper
+
+
+@decorator
+def transformation(n, sep="-"):
     new_str = ""
     for char in n:
         if char in t:
             new_str += t[char]
+        elif char in ": ;.,_":
+            new_str += "-"
         elif char == " ":
             new_str += sep
         else:
@@ -15,7 +29,6 @@ def transformation(n, sep = "-"):
     return new_str
 
 
-n = input().lower()
+s = input().lower()
 
-print(transformation(n))
-print(transformation(n, sep="+"))
+print(transformation(s))
